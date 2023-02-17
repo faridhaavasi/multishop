64e631f6c8c67b2e1ef0224886c41834f29ecca8
faridhaavasi <faridhavasi777@gmail.com>
2023-02-17 17:45:15 +0330

    startapp product

--- multishop/settings.py
+++ multishop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'account.apps.AccountConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
--- multishop/urls.py
+++ multishop/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('product/', include('product.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

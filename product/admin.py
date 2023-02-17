8ae254c93d3accbe52195ba4163f68d8d3f7fb08
faridhaavasi <faridhavasi777@gmail.com>
2023-02-17 18:22:46 +0330

    add model product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Color, Size, Product
 # Register your models here.
+
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Product)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Color(models.Model):
+    title = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title
+
+class Size(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    CHOISES = (
+        ('el', 'electronic'),
+        ('cl', 'clothing'),
+
+    )
+    type = models.CharField(max_length=2, choices=CHOISES)
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/products')
+    price = models.IntegerField()
+    discount = models.SmallIntegerField()
+    color = models.ManyToManyField(Color, related_name='product', blank=True, null=True)
+    size = models.ManyToManyField(Size, related_name='product', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+

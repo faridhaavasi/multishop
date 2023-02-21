23c745c4e04e663b4845bb7e647546a4bc43245e
faridhaavasi <faridhavasi777@gmail.com>
2023-02-21 19:49:04 +0330

    startapp cart

--- multishop/urls.py
+++ multishop/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('product/', include('product.urls')),
+    path('cart/', include('cart.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

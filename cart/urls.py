428fc6519dbc0e74800e988ce7be7b94d5c63038
faridhaavasi <faridhavasi777@gmail.com>
2023-03-01 21:41:49 +0330

    start cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'cart'
 urlpatterns = [
-    path('cartdetail', views.cartdetailview.as_view(), name='cartdetail')
+    path('cartdetail', views.cartdetailview.as_view(), name='cartdetail'),
+    path('addproduct', views.addprofuct.as_view(), name='addproduct'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 # Create your views here.
 
 class cartdetailview(View):
     def get(self, request):
         return render(request, 'cart/cartdetail.html', {})
+
+
+class addprofuct(View):
+    def post(self, request):
+        print('added product ')
+        return redirect('cart:cartdetail')
